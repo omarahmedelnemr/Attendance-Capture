37a04db8d75ae0dd7e40ef349f29c285c0fe7974
omarahmedelnemr <omarahmedelnemr16@gmail.com>
2024-01-03 08:09:09 +0200

    renaming the Models

--- DirectTest.py
+++ DirectTest.py
@@ -9,12 +9,12 @@ def predict_image(image_name):
     image_path = "./uploads/" + image_name
 
     # Load the trained model
-    with open('./Models/model14/friend_face_recognition_model.pkl', 'rb') as model_file:
+    with open('./Models/model10/friend_face_recognition_model.pkl', 'rb') as model_file:
         classifier = pickle.load(model_file)
 
-    # # Load the label encoder
-    # label_encoder = LabelEncoder()
-    # label_encoder.classes_ = np.load('./Models/model7/label_encoder_classes.npy')
+    # Load the label encoder
+    label_encoder = LabelEncoder()
+    label_encoder.classes_ = np.load('./Models/model10/label_encoder_classes.npy')
 
     # Load the image and find face locations and embeddings
     image = face_recognition.load_image_file(image_path)
